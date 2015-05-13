73b7a3d1d24d94fac18e75d4414d63d788ed3625
James F Robison-Cox <jimrc@math.montana.edu>
2015-05-13 10:11:35 -0600

    set spacing

--- navbar/ui.R
+++ navbar/ui.R
@@ -1,83 +1,89 @@
 library(markdown)
 
-shinyUI(navbarPage("Variable Type:",
-                   navbarMenu("One Categorical",
-                              tabPanel("Descriptives",
-                                       verbatimTextOutput("Describe 1 Cat")
-                              ),
-                              tabPanel("Test",
-                                       verbatimTextOutput("Test 1 Proportion")
-                              ),
-                              tabPanel("Estimate",
-                                       verbatimTextOutput("Estimate 1Proportions")
-                              ),
-                              tabPanel("Normal Distribution",
-                                       verbatimTextOutput("Normal Approx")
-                              )
-                    ),
-                    navbarMenu("One Quantitative",
-                               tabPanel("Descriptives",
-                                        verbatimTextOutput("Describe 1 Quantitative")
-                               ),
-                               tabPanel("Test",
-                                        verbatimTextOutput("Test 1 mean")
-                               ),
-                               tabPanel("Estimate",
-                                        verbatimTextOutput("Estimate 1 mean")
-                               ),
-                               tabPanel("Bootstrap Demo",
-                                        verbatimTextOutput("BootStrap Demo")
-                               ),
-                               tabPanel("Normal Distribution",
-                                        verbatimTextOutput("t distribution")
-                               )
-                               
-                    ),
-                   navbarMenu("Two Cat.",
-                              tabPanel("Descriptives",
-                                       verbatimTextOutput("Describe 2 Cat")
-                              ),
-                              tabPanel("Test",
-                                       verbatimTextOutput("Test Equality of 2 Proportions")
-                              ),
-                              tabPanel("Estimate",
-                                       verbatimTextOutput("Estimate Difference in Proportions")
-                              ),
-                              tabPanel("Normal Distribution",
-                                       verbatimTextOutput("Normal Approx")
-                              )
-                   ),
-                   navbarMenu("Two Quant.",
-                              tabPanel("Descriptives",
-                                      verbatimTextOutput("Describe 2 Quant")
-                              ),
-                              tabPanel("Test",
-                                      verbatimTextOutput("Test Slope/Correlation")
-                              ),
-                              tabPanel("Estimate Slope/Correlation",
-                                      verbatimTextOutput("Estimate Difference in Proportions")
-                              ),
-                              tabPanel("t- Distribution",
-                                      verbatimTextOutput("Normal Approx")
-                              )
-                   ),
-                   navbarMenu("One of Each",
-                              tabPanel("Descriptives",
-                                       verbatimTextOutput("Describe")
-                              ),
-                              tabPanel("Test",
-                                       verbatimTextOutput("Test Equality of 2 Means")
-                              ),
-                              tabPanel("Estimate",
-                                       verbatimTextOutput("Estimate Difference in Means")
-                              ),
-                              tabPanel("t Distribution",
-                                       verbatimTextOutput("t Approx")
-                              )
-                    ),
-                   navbarMenu("Theoretical Tools",
-                              tabPanel("Probabilities"),
-                              tabPanel("Power")
-                              )
-                   
-))
+shinyUI(
+  navbarPage("Variable Type:",
+    ####   One Categorical  -----------------------------------
+    navbarMenu("One Categorical",
+       tabPanel("Descriptives",
+          verbatimTextOutput("Describe 1 Cat")
+       ),
+       tabPanel("Test",
+         verbatimTextOutput("Test 1 Proportion")
+       ),
+       tabPanel("Estimate",
+         verbatimTextOutput("Estimate 1Proportions")
+       ),
+       tabPanel("Normal Distribution",
+         verbatimTextOutput("Normal Approx")
+    ),
+    ####   One Quantitative  -----------------------------------
+    navbarMenu("One Quantitative",
+      tabPanel("Descriptives",
+        verbatimTextOutput("Describe 1 Quantitative")
+      ),
+      tabPanel("Test",
+        verbatimTextOutput("Test 1 mean")
+      ),
+      tabPanel("Estimate",
+        verbatimTextOutput("Estimate 1 mean")
+      ),
+      tabPanel("Bootstrap Demo",
+        verbatimTextOutput("BootStrap Demo")
+      ),
+      tabPanel("Normal Distribution",
+        verbatimTextOutput("t distribution")
+      )                         
+    ),
+    
+    ####   Two Categorical  -----------------------------------
+    navbarMenu("Two Cat.",
+      tabPanel("Descriptives",
+        verbatimTextOutput("Describe 2 Cat")
+      ),
+      tabPanel("Test",
+        verbatimTextOutput("Test Equality of 2 Proportions")
+      ),
+      tabPanel("Estimate",
+        verbatimTextOutput("Estimate Difference in Proportions")
+      ),
+      tabPanel("Normal Distribution",
+       verbatimTextOutput("Normal Approx")
+      )
+  ),
+  ####   Two Quantitative -----------------------------------
+  navbarMenu("Two Quant.",
+    tabPanel("Descriptives",
+      verbatimTextOutput("Describe 2 Quant")
+    ),
+    tabPanel("Test",
+      verbatimTextOutput("Test Slope/Correlation")
+    ),
+    tabPanel("Estimate Slope/Correlation",
+      verbatimTextOutput("Estimate Difference in Proportions")
+    ),
+    tabPanel("t- Distribution",
+      verbatimTextOutput("Normal Approx")
+    )
+  ),
+  ####   One of Each  -----------------------------------
+  navbarMenu("One of Each",
+    tabPanel("Descriptives",
+       verbatimTextOutput("Describe")
+    ),
+    tabPanel("Test",
+       verbatimTextOutput("Test Equality of 2 Means")
+    ),
+    tabPanel("Estimate",
+       verbatimTextOutput("Estimate Difference in Means")
+    ),
+    tabPanel("t Distribution",
+       verbatimTextOutput("t Approx")
+    )
+  ),
+  ####   Theory  -----------------------------------
+  navbarMenu("Theoretical Tools",
+    tabPanel("Probabilities"),
+    tabPanel("Power")
+  )       
+ )
+)
