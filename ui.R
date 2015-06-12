6e79b068bf63f7dd038453adb113d69a8d00ba33
Allison Theobold <atheobold@aol.com>
2015-06-12 12:46:26 -0600

    Changes: Action buttons for all 5 shuffles. Problems: Won't compile. Message: "Error in exprToFunction(expr, eval.env, quoted, 2) :   argument "expr_sub" is missing, with no default"

--- ui.R
+++ ui.R
@@ -214,10 +214,11 @@ shinyUI(navbarPage("Intro Stat Apps", id="top-nav", collapsible=TRUE,
                    
                    column(4, 
                           h3("Number of Shuffles"),
-                          radioButtons("num_shuffles", label = NULL, 
-                                       choices = c("1", "10", "100", "1000", "5000"), 
-                                       selected = 1, inline = TRUE), 
-                          actionButton("cat2_shuffle", label = "Shuffle"))
+                          actionButton("cat2_shuffle_1", label = "1"),
+                          actionButton("cat2_shuffle_10", label = "10"),
+                          actionButton("cat2_shuffle_100", label = "100"),
+                          actionButton("cat2_shuffle_1000", label = "1000"),
+                          actionButton("cat2_shuffle_5000", label = "5000"))
                  ),
                  column(4, plotOutput("cat2Test", width = "90%") 
                  ))
