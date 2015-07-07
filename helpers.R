791fb607b035b272abda4644aaccc80c9cfdd280
Allison Theobold <atheobold@aol.com>
2015-07-07 16:30:15 -0600

    New shuffle function working to return matrix of phats and diff in phats.

--- helpers.R
+++ helpers.R
@@ -1,17 +1,18 @@
 ## Function to draw and store difference in proportions for randomization
 
 cat2_test_shuffles <- function(shuffles, y1, y2, n1, n2){
-
-shuffles <- as.numeric(shuffles)
-y1_new <- rhyper(shuffles, y1 + y2, (n1 + n2) - (y1 + y2), n1)
-y2_new <- (y1 + y2) - y1_new
-
-phat.1 <- y1_new/n1
-phat.2 <- y2_new/n2
-diff.p <- phat.1 - phat.2
-diff.p <- as.matrix(diff.p, ncol = 1, nrow = shuffles)
-
-return(diff.p)
+  
+  shuffles <- as.numeric(shuffles)
+  y1_new <- rhyper(shuffles, y1+y2,(n1 + n2) - (y1 + y2), n1)
+  y2_new <- (y1 + y2) - y1_new
+  
+  
+  phat.1 <- y1_new/n1
+  phat.2 <- y2_new/n2
+  diff.p <- phat.1 - phat.2
+  data <- as.matrix(cbind(phat.1, phat.2, diff.p), ncol = 3, nrow = shuffles)
+  
+  return(data)
 }
 
 ## Function to draw and store difference in proportions for bootstrap
